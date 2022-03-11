268cfc2b51ea886f6499704e4b29487785564db6
Olayinka <90999654+RAYN689@users.noreply.github.com>
2022-03-11 18:20:27 +0000

    my database connection and user registration or login details stored in the local database.
    Created extra functions for the login process
    
    Trying to sort out an issue of my select program displaying an empty list
    
    > output the database search with the query from python script through the use parameterised query
    
    > converted the result type into a tuple
    
    > accessed the different items in the tuple and stored them in variables
    
    > successfully-built the if statement that checks the database and logs the user in or not.
    
    >Login system complete!

--- loginsystem.py
+++ loginsystem.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import mysql.connector
 import os
-import sqlite3
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,40 +13,37 @@ Kindly Fill in the following details to Get registered.
 Thanks''')
 
 
-def login_access():
-    user = int(input('Press 1 if you are a new user and 2 for an old user:'))
-    if user == 1:
-        store_user_input()
-    elif user == 2:
-        print('Enter your user name and password to proceed')
-
-
 # create connection object to the sql local database
+# create a cursor object for connection
 # use the dotenv method to hide important information
+
+
 connection = mysql.connector.connect(host=os.getenv("HOST"),
                                      user=os.getenv("USERNAME"),
                                      password=os.getenv("PSWD"),
                                      port=os.getenv("PORT"),
                                      auth_plugin='mysql_native_password',
                                      database='Loginportal')
-# create a cursor object
-mycursor = connection.cursor()
 
+mycursor = connection.cursor()
 
 # create a database called Loginportal and table user
 # mycursor.execute('CREATE DATABASE Loginportal')
 # create user table and insert columns into it
-def create_table():
-    mycursor.execute('''CREATE TABLE user (
-                        user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-                        user_name VARCHAR(50),
-                        password VARCHAR(20),
-                        username VARCHAR(30),
-                        year_of_birth INTEGER,
-                        department VARCHAR(20),
-                        gender VARCHAR(15)
-                    )''')
 
+# create_table = mycursor.execute('''CREATE TABLE course (
+#                         user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+#                         user_name VARCHAR(50),
+#                         password VARCHAR(20),
+#                         username VARCHAR(30),
+#                         year_of_birth INTEGER,
+#                         department VARCHAR(20),
+#                         gender VARCHAR(15)
+#                         )''')
+#
+# connection.commit()
+
+# create_table()
 
 # sql query that inserts values into the database
 sql = '''INSERT INTO user (user_name,password,fullname,year_of_birth,department,gender)
@@ -79,6 +75,20 @@ def login_page():
     login_access()
 
 
+def login_access():
+    user = int(input('Press 1 if you are a new user and 2 for an old user:'))
+    if user == 1:
+        store_user_input()
+        print(input('PRESS ENTER TO RETURN TO LOGIN PAGE:'))
+        print('Please enter your username and password')
+        pass
+
+    elif user == 2:
+        print('Enter your user name and password to proceed')
+        #pass
+        #user_login()
+login_access()
+
 # using a parameterised query to store variable in sql
 sql_select = '''
                 SELECT user_name, password, fullname
@@ -87,30 +97,35 @@ sql_select = '''
                 AND password= %s
             '''
 
+
 username = str(input('Please enter your username:'))
 password = str(input('Please enter your password:'))
-
 mycursor = connection.cursor()
-result = mycursor.execute(sql_select, (username, password,))
+result = mycursor.execute(sql_select, (username, password), )
 result = mycursor.fetchall()
-
-# converts the result set from a list to a tuple
 result_tuple = tuple(result)
-print(result_tuple)
-print(type(result_tuple))
 
 
-# access the elements of the result and assign it to a variable
-def split_result():
+def login_access():
     db_username = result_tuple[0][0]
     db_password = result_tuple[0][1]
     if username == db_username and password == db_password:
         print('Login Successful!' '\n'
-              'Welcome' '!')
+              'Welcome', '!')
+        print(input('Press ENTER to continue'))
 
 
-if result_tuple == ():
-    print('login unsuccessful')
-else:
-    split_result()
-# # df = pd.read_sql(result, connection, encoding='unicode_escape')
+
+
+# checks user details and allows login or not
+def user_login():
+    if result_tuple == ():
+        print('Username or password does not match.' '\n'
+              'login unsuccessful, Please try again')
+    else:
+        login_access()
+
+user_login()
+
+
+# create a while loop to limit number of login attempts
